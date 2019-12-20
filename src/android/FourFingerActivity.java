e7edd13dfb363a81811031ea30aa684e31890693
rtamayov <robert.tamayo.valdivia@gmail.com>
2019-12-20 09:34:53 -0500

    se puso los aar

--- src/android/FourFingerActivity.java
+++ src/android/FourFingerActivity.java
@@ -5,20 +5,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
+
+import com.veridiumid.sdk.IVeridiumSDK;
 
 public class FourFingerActivity extends Activity {
 
     private String package_name;
 	private Resources resources;
 	
+	public Button button_capture;
+
+	private IVeridiumSDK mBiometricSDK;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		setContentView(getResourceId("layout/activity_fourfinger"));
+		
+		
+		button_capture = (Button) findViewById(getResourceId("id/btn_presioname"));
+		button_capture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.v("XXX","Hole a Todes!!");
+            }
+        });
     }
 	
 	@Override
@@ -35,6 +49,7 @@ public class FourFingerActivity extends Activity {
         if(resources == null) resources = getApplication().getResources();
         return resources.getIdentifier(typeAndName, null, package_name);
     }
+
 
 
 }
