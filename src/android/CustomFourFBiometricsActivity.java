4694d48604e1f4f17cf7bf549c9542aa59f94e34
rtamayov <robert.tamayo.valdivia@gmail.com>
2019-12-20 14:21:11 -0500

    Todo Back

---
+++ src/android/CustomFourFBiometricsActivity.java
@@ -0,0 +1,13 @@
+package bio.insolutions.veridium.entel.piloto.cordova.plugin;
+
+import android.app.Activity;
+
+public class CustomFourFBiometricsActivity extends Activity{
+	
+	
+	
+	
+	
+	
+	
+}
